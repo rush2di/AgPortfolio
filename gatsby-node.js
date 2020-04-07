aa044e82e4f3de7ea95097e41139c9f3d10b42b2
Rochdi <roch2di@gmail.com>
2020-04-07 16:08:58 +0200

    --Update-init-plugins

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,19 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const axios = require("axios")
 
-// You can delete this file if you're not using it
+// const url =
+//   "https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=731f6d52097190e3d99faa37716978fd&photoset_id=72157713767713111&user_id=155026906%40N08&format=json&nojsoncallback=1"
+// const albumUrl =
+//   "https://www.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=731f6d52097190e3d99faa37716978fd&user_id=155026906%40N08&format=json&nojsoncallback=1"
+
+// let albumsFetcher = async () => {
+//   const res = await axios.get(albumUrl)
+//   console.log("albums", res.data.photosets.photoset)
+// }
+// let photosetFetcher = async () => {
+//   const res = await axios.get(url)
+//   console.log(res.data.photoset.photo)
+// }
+// albumsFetcher()
+// photosetFetcher()
+
+// imgSrc = https://live.staticflickr.com/${server}/${id}_${secret}.jpg
