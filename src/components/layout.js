1c35f0275294da4868c8b1710ffe736f31c3e727
Rochdi <roch2di@gmail.com>
2020-04-07 19:22:56 +0200

    --Add-border-styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,28 @@ import "../styles/styles.scss"
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
-      <div className="">
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
+      <div className="app--wrapper">
+        <nav className="app--nav container">
+          <div className="navlogo">
+            <span>Grana.ab</span>
+          </div>
+          <ul className="navitems">
+            <li>Home</li>
+            <li>Blog</li>
+          </ul>
+        </nav>
+        <main className="container--main">{children}</main>
+        <footer className="container--footer">
+          © {new Date().getFullYear()}, developed by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.github.com/rush2di">rush2di</a>
         </footer>
+        <div className="borders">
+          <div className="top thickness-hight"></div>
+          <div className="bottom thickness-hight"></div>
+          <div className="left thickness-width"></div>
+          <div className="right thickness-width"></div>
+        </div>
       </div>
     </React.Fragment>
   )
