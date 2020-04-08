2a42353efef60486db74f75b1bc5ddb92bbdc561
Rochdi <roch2di@gmail.com>
2020-04-08 18:55:19 +0200

    Init netlify-cms

---
+++ src/cms/cms.js
@@ -0,0 +1,7 @@
+import CMS from "netlify-cms-app"
+
+// import PostPreview from "./previews/postPreview"
+// import InformationPreview from "./previews/informationPreview"
+
+// CMS.registerPreviewTemplate("articles", PostPreview)
+// CMS.registerPreviewTemplate("apropos", InformationPreview)
