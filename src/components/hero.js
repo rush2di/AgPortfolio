3782c4735ad61b9bc22440cf8d80fb7d20debc35
Rochdi <roch2di@gmail.com>
2020-04-09 20:39:30 +0200

    Update components and init Gsap

---
+++ src/components/hero.js
@@ -0,0 +1,130 @@
+import PropTypes from "prop-types"
+import React, { useEffect, useLayoutEffect } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import gsap from "gsap"
+
+const Hero = ({ siteTitle }) => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      {
+        allMarkdownRemark(
+          filter: {
+            frontmatter: { templateKey: { eq: "home-page-customizer" } }
+          }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                introTxt
+                profileImg {
+                  childImageSharp {
+                    fluid {
+                      src
+                      srcSet
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const { introTxt: txt } = allMarkdownRemark.edges[0].node.frontmatter
+  const {
+    fluid: img,
+  } = allMarkdownRemark.edges[0].node.frontmatter.profileImg.childImageSharp
+
+  return (
+    <React.Fragment>
+      <HeroContent txt={txt} img={img} />
+    </React.Fragment>
+  )
+}
+
+export const HeroContent = ({ txt, img, cmsPreviewImg }) => {
+  const title = React.useRef()
+  const subTitle = React.useRef()
+  const imageMask = React.useRef()
+  const image = React.useRef()
+  const introduction = React.useRef()
+
+  useLayoutEffect(() => {
+    gsap.from(title.current, {
+      duration: 1,
+      y: "100%",
+      delay: 0.3,
+      ease: "power3.out",
+    })
+    gsap.from(subTitle.current, {
+      duration: 1,
+      y: "100%",
+      delay: 0.8,
+      ease: "power3.out",
+    })
+    gsap.to(imageMask.current, {
+      duration: 1.5,
+      x: "-100%",
+      delay: 1.5,
+      ease: "power3.inOut",
+    })
+    gsap.from(image.current, {
+      duration: 1.5,
+      scale: 1.3,
+      delay: 1.8,
+      ease: "power3.out",
+    })
+    gsap.from(introduction.current, {
+      duration: 1,
+      y: 10,
+      delay: 1,
+      ease: "power3.out",
+      opacity: 0,
+    })
+  })
+
+  return (
+    <div className="section-hero">
+      <div className="section-hero--grid">
+        <div className="hero-grid-one">
+          <div className="hero-title-container">
+            <div className="hero-title">
+              <h1>
+                <span ref={title}>Abderrahmane</span>
+              </h1>
+              <h1>
+                <span ref={subTitle}>Grana</span>
+              </h1>
+            </div>
+          </div>
+          <p ref={introduction}>{txt}</p>
+        </div>
+        <div className="hero-grid-two">
+          <div className="hero-image-container">
+            <div className="hero-image-mask">
+              <img
+                ref={image}
+                src={img.src ? img.src : img}
+                srcSet={img.srcSet && img.srcSet}
+                alt=""
+              />
+              <div ref={imageMask} className="testMask"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Hero.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Hero.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import HomePage from "../templates/homePage"
 
 const IndexPage = () => (
   <Layout>
-    <div className="section-hero">
-      <div className="section-hero--grid">
-        <div className="hero-grid-one">
-          <div className="hero-title-container">
-            <div className="hero-title">
-              <h1>
-                <span>Abderrahmane</span>
-              </h1>
-              <h1>
-                <span>Grana</span>
-              </h1>
-            </div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod temaliqua, enim ad minim veniam
-          </p>
-        </div>
-        <div className="hero-grid-two">
-          <div className="hero-image-container">
-            <div className="hero-image-mask">
-              <div className="testImg"></div>
-              <div className="testMask"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <HomePage />
   </Layout>
 )
 
