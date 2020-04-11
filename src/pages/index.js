7240888ad03f5a4053855e706234f0b58d251653
Rochdi <roch2di@gmail.com>
2020-04-11 16:38:02 +0200

    Fix home page structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import HomePage from "../templates/homePage"
-import Hero from "../components/hero"
 
 const IndexPage = () => (
   <Layout>
-  	<Hero />
     <HomePage />
   </Layout>
 )
