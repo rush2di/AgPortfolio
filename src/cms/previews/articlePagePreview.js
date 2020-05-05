164944f906f4d524bdf4ac3f1c48ffc1adcc4628
Rochdi <roch2di@gmail.com>
2020-05-05 16:36:08 +0200

    Fix article preview component

--- src/cms/previews/articlePagePreview.js
+++ src/cms/previews/articlePagePreview.js
@@ -1,58 +1,23 @@
-import React, { Component } from "react"
+import React from "react"
 
 import { ArticleTemplate } from "../../templates/articlePage"
 
-class ArticlePreview extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			src: "",
-		}
-	}
-	subscribed = true
+const ArticlePreview = (props) => {
+  const date = "day month year"
+  const title = props.entry.getIn(["data", "title"])
+  const tagsCheck = props.entry.getIn(["data", "tags"])
+  const tags = tagsCheck ? tagsCheck.toJS() : ["tags go here"]
+  const bgImage = props.entry.getIn(["data", "cover"])
 
-	fetchAsset = () => {
-		const path = this.props.entry.getIn(["data", "cover"])
-		path &&
-			this.props.getAsset(path).then(res => {
-				if (this.subscribed) {
-					this.setState({ src: res.toString() })
-				}
-			})
-	}
-
-	componentDidMount() {
-		this.fetchAsset()
-	}
-
-	componentWillUnmount() {
-		this.subscribed = false
-	}
-
-	componentDidUpdate(prevProps) {
-		const prevPath = prevProps.entry.getIn(["data", "cover"])
-		const path = this.props.entry.getIn(["data", "cover"])
-		if (prevPath !== path || prevProps.getAsset !== this.props.getAsset) {
-			this.fetchAsset()
-		}
-	}
-
-	render() {
-		const title = this.props.entry.getIn(["data", "title"])
-		const date = this.props.entry.getIn(["data", "date"])
-		const tagsCheck = this.props.entry.getIn(["data", "tags"])
-		const tags = tagsCheck ? tagsCheck.toJS() : ["tags go here"]
-		const html = this.props.widgetFor("body")
-		const bgImage = this.state.src
-		
-		return (
-			<React.Fragment>
-				<ArticleTemplate {...{title, date, tags, bgImage}} isPreview={true} >
-					<div className="article_body">{html}</div>
-				</ArticleTemplate>
-			</React.Fragment>
-		)
-	}
+  return (
+    <React.Fragment>
+      <div className="section">
+        <ArticleTemplate {...{ title, tags, date, bgImage }} isPreview={true}>
+          <div className="article_body">{props.widgetFor("body")}</div>
+        </ArticleTemplate>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default ArticlePreview
